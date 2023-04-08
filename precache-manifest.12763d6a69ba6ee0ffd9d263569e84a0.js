7733c44e750128c47c87e0f3daa2bb1bb4f1969d
Code Sandbox <hello@codesandbox.io>
2023-04-08 18:44:01 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.12763d6a69ba6ee0ffd9d263569e84a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac39f1404044ae3b627cfb566c01c85",
+    "url": "https://pankaj-2503.github.io/csb-np1tr2/index.html"
+  },
+  {
+    "revision": "ad36e0aba4588e846032",
+    "url": "https://pankaj-2503.github.io/csb-np1tr2/static/js/2.05d131db.chunk.js"
+  },
+  {
+    "revision": "5271b4f10f182340c705",
+    "url": "https://pankaj-2503.github.io/csb-np1tr2/static/js/main.084ccdc7.chunk.js"
+  },
+  {
+    "revision": "97576a63a91d12f83860",
+    "url": "https://pankaj-2503.github.io/csb-np1tr2/static/js/runtime-main.3d81517a.js"
+  }
+]);
